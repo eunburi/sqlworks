d7dad5a2261760676812b2594f5c8ee2346b115d
eunburi <finestar23@naver.com>
2023-08-03 18:32:34 +0900

    0803 new mysql board322

---
+++ mysqlworks/dept_emp.sql
@@ -0,0 +1,50 @@
+-- 부서와 사원 테이블 만들기
+use mydb;
+-- 부서 테이블
+create table department(
+	deptid int primary key,  		-- 부서번호
+    deptname  varchar(30) not null, -- 부서이름
+    location  varchar(20) not null 	-- 사무실위치
+);
+
+-- 사원 테이블
+CREATE TABLE employee(
+	empid  INT PRIMARY KEY,			-- 사원아이디
+    empname VARCHAR(30) NOT NULL,  	-- 사원이름
+    age INT,                        -- 나이 
+    deptid INT NOT NULL,            -- 부서아이디(외래키)
+    constraint fk_dept_employee foreign key(deptid)
+    references department(deptid)  -- 관계 설정(외래키)
+);
+
+-- 부서 추가
+insert into department values(10, '개발팀', '서울');
+INSERT INTO department VALUES(20, '디자인팀', '성남');
+
+-- 사원 추가
+insert into employee values(101, '한라산', 27, 10);
+insert into employee values(102, '동해', 30, 20);
+insert into employee values(103, '서해', 23, 30);  -- 부모키가 없음(제약 위배)
+
+-- 부서 삭제 ( 부서 이름이 '디자인팀'을 삭제 - > 자식 로우(row)가 있으므로 삭제 안됨)
+-- 제약조건(on delete cascade)을 추가하면, 삭제 가능함
+delete from department where deptid = 20;
+
+-- 부서 검색
+select * from department;
+-- 사원 검색
+select * from employee;
+
+-- 사원이름이 '한라산'인 사원의 사원아이디와 부서아이디를 출력하시오
+select empid, deptid from employee where empname= '한라산';
+
+-- 부서이름이 '개발팀'인인 사무실의 위치를 검색하시오
+select location from department where deptname = '개발팀';
+
+-- 사원번호가 102인 사람의 이름을 '동해'로 나이를 '30'세로 변경하시오
+-- update ~ set ~ where 구문
+update employee set empname = '동해',age = 30
+where empid = 102;
+
+-- 트랜잭션(Transaction) - (실행-commit, 취소 - rollback)
+rollback;
--- oracleworks/mydb/t_member_board.sql
+++ oracleworks/mydb/t_member_board.sql
@@ -24,8 +24,18 @@ CREATE TABLE t_board(
 );
 
 --글번호,글제목,글내용,작성일,수정일,조회수,아이디(외래키)
---자동 순번(SEQUENCE)
-CREATE SEQUENCE b_seq;
+--자동 순번(SEQUENCE) 전체 코드(CACHE 기본값 20 - 캐시 사용 안함)
+CREATE SEQUENCE b_seq
+INCREMENT BY 1
+START WITH 1
+MINVALUE 1
+MAXVALUE 9999
+NOCYCLE
+NOCACHE;
+
+-- 파일 업로드 칼럼 추가
+ALTER TABLE t_board ADD fileupload VARCHAR2(100);
+
 
 INSERT INTO t_member(memberid, passwd, name, gender)
 VALUES ('cloud', 'm12345', '조은별', '여' );
@@ -38,7 +48,41 @@ COMMIT;
 SELECT * FROM t_member;
 SELECT * FROM t_board;
 
+-- id중복체크
+SELECT COUNT(*) AS result
+FROM t_member WHERE memberid = 'cloud';
 
-DROP SEQUENCE b_seq; -- 시퀀스 삭제
+-- id중복확인
+SELECT DECODE(COUNT(*), 1, 'true', 'false') AS result
+FROM t_member WHERE memberid = 'cloud';
+
+-- 페이지 처리
+SELECT ROWNUM, t_board.*
+FROM t_board 
+WHERE ROWNUM >= 1 AND ROWNUM <=10
+ORDER BY bnum;
+
+-- 페이지 처리 (인라인뷰 - 중첩쿼리(서브쿼리)
+select * 
+FROM (SELECT ROWNUM RN, t_board.*
+FROM t_board ORDER BY bnum DESC)
+WHERE RN >= 11 AND RN <=20; -- 별칭(RN)을 사용해야 가능
+
+-- 페이지 처리2 (인라인뷰 - 중첩쿼리(서브쿼리)
+select * 
+FROM (SELECT ROWNUM RN, board.*
+        FROM(SELECT * FROM t_board ORDER BY bnum DESC) board)
+WHERE RN >= 1 AND RN <=10; -- 별칭(RN)을 사용해야 가능
+
+
+--총 행의 수
+SELECT COUNT(*) FROM t_board;
+
+
+
+
+--ORDER BY bnum;
+
 DROP TABLE t_board;  -- board 테이블 삭제
+DROP SEQUENCE b_seq; -- 시퀀스 삭제
 
